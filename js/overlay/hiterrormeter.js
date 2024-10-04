9f85fde0e35c42cbe1612a3519f000f8ac14fce0
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-04 18:52:00 +0000

    Please

--- js/overlay/hiterrormeter.js
+++ js/overlay/hiterrormeter.js
@@ -24,7 +24,7 @@ define([], function () {
          piece.height = 12;
          piece.anchor.set(0.5);
          piece.x = 0;
-         piece.y = (barheight + 10) * pos;
+         piece.y = barheight * pos;
          return piece;
       };
       let newbarpiece = function(height, tint) {
